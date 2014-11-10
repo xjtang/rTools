6e5561c84fa95b862b24c9901accae5497118ad2
Xiaojing Tang <xjtang@users.noreply.github.com>
2014-11-09 23:35:11 -0500

    Update web_tools.R

--- web_tools.R
+++ web_tools.R
@@ -27,3 +27,10 @@ binDownload <- function(url,output){
   writeBin(bin,output)
   return(0)
 }
+
+# source a url (by Tony Breyal originally)
+sourceURL <- function(url){
+  script - getURL(url,ssl.verifypeer=F)
+  eval(parse(text=script),envir=.GlobalEnv)
+  return(0)
+}
