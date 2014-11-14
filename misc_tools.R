06eb3113a8cade735d105d0136959f8af5e55442
Xiaojing Tang <xjtang@users.noreply.github.com>
2014-11-14 15:38:08 -0500

    Update misc_tools.R

--- misc_tools.R
+++ misc_tools.R
@@ -26,3 +26,87 @@ showProgress <- function(i,n,step){
   }
   return(percent)
 }
+
+# calculate day of year from date
+dateToDOY <- function(year,month,day){
+
+  # check year
+  if(year<0){
+    cat('check the year\n')
+    return(0)
+  }
+  
+  # 365 days or 366 days in that year
+  if((year%%4==0&year%%100!=0)|year%%400==0){
+    feb <- 29
+  }else{
+    feb <- 28
+  }
+  dom <- c(31,feb,31,30,31,30,31,31,30,31,30,31)
+  
+  # check month
+  if(month>12|month<1){
+    cat('check the month\n')
+    return(0)
+  }
+  
+  # check day
+  if(day<1|day>dom(month)){
+    cat('check the day\n')
+    return(0)
+  }
+  
+  # calculate doy
+  if(month==1){
+    doy <- year*1000 + day
+  }else{
+    doy <- year*1000 + sum(dom[1:(month-1)]) + day
+  }
+  
+  # done
+  return(doy)
+}
+
+# calculate the date from day of year
+doyToDate <- function(doy){
+  
+  # get year
+  year <- floor(doy/1000)
+  
+  # check the doy
+  if(year<0){
+    cat('invalid doy\n')
+    return(0)
+  }
+  
+  # 365 days or 366 days in that year
+  if((year%%4==0&year%%100!=0)|year%%400==0){
+    feb <- 29
+  }else{
+    feb <- 28
+  }
+  dom <- c(31,feb,31,30,31,30,31,31,30,31,30,31)
+  
+  # get total days
+  tday <- doy-year*1000
+  
+  # check total days
+  if(tday<=0|tday>sum(dom)){
+    cat('invalid doy\n')
+    return(0)
+  }
+  
+  # calculate month and day
+  for(i in 1:12){
+    tday <- tday-dom(i)
+    if(tday<=0){
+      month <- i
+      day <- tday+dom(i)
+      break
+    }
+  }
+  
+  # done
+  date <- c(year,month,day)
+  return(date)
+}
