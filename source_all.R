7a6f74c12e4f18aea62871927dff2d340c26eb68
Xiaojing Tang <xjtang@users.noreply.github.com>
2014-11-30 14:45:19 -0500

    Update source_all.R

--- source_all.R
+++ source_all.R
@@ -1,9 +1,10 @@
 # source_all.R
-# Version 1.0
+# Version 1.1
 #
 # Project: Fusion
 # By Xiaojing Tang
 # Created On: 11/13/2014
+# Last Update: 11/30/2014
 #
 # Input Arguments: NA
 #   
@@ -16,6 +17,9 @@
 # Version 1.0 - 11/13/2014
 #   This script sources all tools in this repo.
 #   
+# Updates of Version 1.1 - 11/30/2014
+#   1.the script now removes unneeded variable.
+#
 # Created on Github on 11/13/2014, check Github Commits for updates afterwards.
 #----------------------------------------------------------------
 
@@ -25,6 +29,7 @@ library(RCurl)
 # sourcing
 script <- getURL('https://raw.githubusercontent.com/xjtang/rTools/master/web_tools.R',ssl.verifypeer=F)
 eval(parse(text=script),envir=.GlobalEnv)
+rm(script)
 sourceURL('https://raw.githubusercontent.com/xjtang/rTools/master/text_tools.R')
 sourceURL('https://raw.githubusercontent.com/xjtang/rTools/master/misc_tools.R')
 
